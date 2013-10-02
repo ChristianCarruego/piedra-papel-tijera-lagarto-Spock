f299823e87aae8276ffbfe5900bdda7b2b382d51
ChristianCarruego <christian.carruego.74@gmail.com>
2013-10-02 00:13:19 -0300

    primera solucion

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,19 +1,24 @@
 package edu.tallerweb.pptls;
 
+import java.util.ArrayList;
 /**
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
+	private final Forma forma;
+	private final ArrayList<Forma> ganaA= new ArrayList<Forma>();
+	
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.forma=forma;
+		
+		
+		
 	}
-
 	/**
 	 * Evaluará el resultado de la partida según las reglas
 	 * del juego.
@@ -21,7 +26,16 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		
+		Integer diferencia;
+		diferencia=(this.forma.ordinal()-otra.forma.ordinal());
+		
+		
+		if (diferencia==0){
+			return Resultado.EMPATA;
+		}
+		else if (diferencia==-4||diferencia==-3||diferencia==1||diferencia==2)
+			return Resultado.GANA;
+		return Resultado.PIERDE;
 	}
-
 }
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PartidaTests {
-
+	/**
+	 * Test de Spock
+	 */
 	@Test
 	public void queSpockVaporizaPiedra() {
 
@@ -15,5 +17,63 @@ public class PartidaTests {
 				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
 
 	}
+	
+	@Test
+	public void SpockEsRefutadoPorPapel() {
 
+		Mano jugadorUno = new Mano(Forma.SPOCK);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Papel refuta a Spock",
+				Resultado.PIERDE, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	
+	@Test
+	public void spockContraSpock() {
+
+		Mano jugadorUno = new Mano(Forma.SPOCK);
+		Mano jugadorDos = new Mano(Forma.SPOCK);
+
+		assertEquals("Spock versus Spock",
+				Resultado.EMPATA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	
+	/**
+	 * Test de Papel
+	 */
+	@Test
+	public void papelEnvuelvePiedra() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Papel envuelve a la piedra",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	
+	@Test
+	public void lagartoComePapel() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Papel es comido por el Largarto",
+				Resultado.PIERDE, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	
+	@Test
+	public void PapelEmpataPapel() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Papel versus Papel",
+				Resultado.EMPATA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	
 }
