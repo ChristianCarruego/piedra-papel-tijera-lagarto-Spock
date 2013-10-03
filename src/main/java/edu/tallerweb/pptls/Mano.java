f22a0656d91812c531376e2ea90ba8b931296d67
ChristianCarruego <christian.carruego.74@gmail.com>
2013-10-02 21:21:03 -0300

    quinta solucion

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -7,8 +7,7 @@ public class Mano {
 	private final Forma forma;
 
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será la que determine su
-	 * condición en el juego. 
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su condición en el juego. 
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
@@ -18,7 +17,6 @@ public class Mano {
 
 	/**
 	 * Evaluará el resultado de la partida según las reglas del juego.
-
 	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
