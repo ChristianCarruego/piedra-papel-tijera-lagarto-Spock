fa1243c30724a0af02b8262ba96ed2a2ebe12e4c
ChristianCarruego <christian.carruego.74@gmail.com>
2013-10-03 01:52:51 -0300

    me duermooo

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,25 +1,17 @@
 ﻿package edu.tallerweb.pptls;
 
-import java.util.ArrayList;
-
 /**
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
 	private final Forma forma;
-	private final ArrayList<Integer> ganarA = new ArrayList<Integer>(){
-		{add(-4);
-		 add(-3);
-		 add (1);
-		 add (2);
-		}
-	};
-	
 
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será la que determine su
 	 * condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
 		this.forma = forma;
@@ -28,7 +20,9 @@ public class Mano {
 
 	/**
 	 * Evaluará el resultado de la partida según las reglas del juego.
-	 * @param otra, la otra Mano.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
@@ -38,10 +32,9 @@ public class Mano {
 
 		if (diferencia == 0) {
 			return Resultado.EMPATA;
-		}
-		if (this.ganarA.contains(diferencia)) {
+		} else if (diferencia == -4 || diferencia == -3 || diferencia == 1
+				|| diferencia == 2)
 			return Resultado.GANA;
-		}
 		return Resultado.PIERDE;
 	}
 }
